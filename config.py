5eec4db79fd2db44f9e3caf287cdca7e734a2989
Elis Sammi <62981106+elissammi@users.noreply.github.com>
2023-05-03 21:50:21 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "employee.c4gehvhkqswz.us-east-1.rds.amazonaws.com"
 customuser = "Cloud_g2"
 custompass = "Cloud_g2"
-customdb = "employee, leave_application , payroll, attendance"
+customdb = ["employee", "leave_application" , "payroll", "attendance"]
 custombucket = "tengwaileong-employee1"
 customregion = "US East (N. Virginia) us-east-1"
